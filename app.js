3bf6ce0e9e275e1e0286df9af0769b6ce2f6bbaa
ivito123 <ivonazarenoiommi@gmail.com>
2023-08-07 15:25:26 -0300

    #

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT= process.env.PORT || 3001;
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(PORT, () => {
+    console.log(`Aplicación esuchando en puerto ${PORT}`);
+});
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+
